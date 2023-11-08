89de278839ce8cadcf8ff671be185ccf1a86ba33
nico <nico@Gabrielas-MacBook-Pro-2.local>
2023-11-08 20:00:28 +0100

    c07, ex01 an 02

--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: nico <nico@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 18:30:57 by nico              #+#    #+#             */
-/*   Updated: 2023/11/08 19:22:41 by nico             ###   ########.fr       */
+/*   Updated: 2023/11/08 19:42:40 by nico             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,7 @@ int	*ft_range(int min, int max)
 	i = 0;
 	if (max <= min)
 		return (NULL);
-	while ((i + min) < max)
-		i++;
-	printf("min: %d\n", min);
-	exit = malloc(i * sizeof(int));
+	exit = malloc((max - min) * sizeof(int));
 	i = 0;
 	while ((i + min) < max)
 	{
--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -6,11 +6,30 @@
 /*   By: nico <nico@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 19:24:39 by nico              #+#    #+#             */
-/*   Updated: 2023/11/08 19:26:16 by nico             ###   ########.fr       */
+/*   Updated: 2023/11/08 19:57:49 by nico             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_ultimate_range(int **range, int min, int max)
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
 {
-	
+	int	i;
+
+	if (max <= min)
+	{
+		*range = NULL;
+		return (0);
+	}
+	*range = malloc((max - min) * sizeof(int));
+	if (range == NULL)
+		return (-1);
+	i = 0;
+	while ((i + min) < max)
+	{
+		*(*range + i) = (i + min);
+		i++;
+	}
+	return (max - min);
 }
