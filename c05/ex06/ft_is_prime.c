9ee6eb052b0bc4dc5818b22a7348fa8c373c38aa
nico <nico@Gabrielas-MacBook-Pro-2.local>
2023-11-08 14:44:13 +0100

    i made ex06

--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -6,10 +6,31 @@
 /*   By: nico <nico@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 13:47:13 by nico              #+#    #+#             */
-/*   Updated: 2023/11/08 13:53:01 by nico             ###   ########.fr       */
+/*   Updated: 2023/11/08 14:43:46 by nico             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// int ft_is_prime(int nb)
+int	ft_is_prime(int nb)
+{
+	int	count;
+
+	count = 2;
+	if (nb <= 1)
+		return (0);
+	if (nb == 2)
+		return (1);
+	while (nb % count != 0)
+	{
+		count++;
+		if (count == nb)
+			return (1);
+	}
+	return (0);
+}
+
+// #include <stdio.h>
+// int main(int argc, char const *argv[])
 // {
+// 	printf("\n1 == primo , 0 == no primo -RESULTADO: %d\n\n", ft_is_prime(1));
+// 	return 0;
 // }
